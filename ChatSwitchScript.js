714842b5be929e4bae6702bf7eb1319c09966c67
Hooijdonk <melissa.van.hooijdonk@ciber.nl>
2021-09-30 12:15:31 +0200

    chatswitch

--- ChatSwitchScript.js
+++ ChatSwitchScript.js
@@ -3,36 +3,33 @@ if (localStorage.getItem("UserRole") == "admin") {
 }
 
 function GetInfo() {
-    fetch('https://localhost:44372/api/Stage')
-         .then((response) => response.json())  //What's the difference 
-        .then(function(returndata) {
+    fetch('https://1a63a4ab-7d00-4b50-aac3-f73fb23a4d1f.mock.pstmn.io/api/Stage')
+        .then((response) => response.json()) //What's the difference 
+        .then(function (returndata){
             console.log(returndata);
-        if(returndata.success){
-            var row="";
-         returndata.data.forEach(function(stage){
-                row +="<tr onclick = "gotostage(/" + returndata.stage");
-                row += "<td>"+ stage.stageName+ "</td>";
-                row += "<td>"+ stage.currentSong+ "</td>";
-                row += "<td>"+ stage.users+ "</td>";
-                row += "<td>"+ stage.JoinStage+ "</td>";
-                row += "</tr>"
-                console.log(row)
-                
+            if (returndata.success) {
+                var row = "";
+                returndata.data.forEach(function (stage) {
+                    row += "<tr onclick='GoToStage("+stage.stageid+")'>";
+                    
+                    row += "<td>" + stage.stageName + "</td>";
+                    row += "<td>" + stage.currentSong + "</td>";
+                    row += "<td>" + stage.users + "</td>";
+                    row += "</tr>";
+                    console.log(row);
 
 
-            })
-            document.getElementById("myData").innerHTML = row;
 
-        }
-        else{
+                }); document.getElementById("myData").innerHTML = row;
 
-        }
-            
+            } else {
+
+            }
+
 
         })
         .catch(error => {
-                console.error("Error");
+            console.error("Error", error);
 
-            });
+        });
 }
-
