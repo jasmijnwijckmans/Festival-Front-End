9769c8d4efc04c85bde25ce7864a2654ad2ae703
jasmijnwijckmans <jasmijn.wijckmans@ziggo.nl>
2021-10-08 08:33:57 +0200

    styling switch

--- Boot/BootScripts/ChatSwitchScriptBoot.js
+++ Boot/BootScripts/ChatSwitchScriptBoot.js
@@ -13,7 +13,7 @@ function GetInfo() {
                 var row = "";
                 returndata.data.forEach(function (stage) {
                     //specific rows and the info for the card
-                    row += " <div class='col-6'>"
+                    row += " <div class='col-3'>"
                     row += " <div class='card mt-3'>"
                     row += "<img class=\"card-img-top\" src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIM3la9vbgy48Y6C_mJHPnQu_CRp1EvjnIfw&usqp=CAU\" alt=\"Card image cap\">"
                     row += "<div>" + stage.stageName + "<br>" + stage.currentSong + "<br>" + stage.numberOfUsers + "<br> <a><button class='btn' value='Join'  onclick='UpdateActivity(" + stage.stageID + ")'> Join<i class=''></i></button></a></div>"
@@ -22,11 +22,11 @@ function GetInfo() {
                     //console.log(row);
                 });
                 if (localStorage.getItem("UserRole") == "admin") { // when userID is admin, show this table.
-                    row += " <div class='col-6'>"
+                    row += " <div class='col-3'>"
                     row += " <div class='card mt-3'>"
                     row += "<img class=\"card-img-top\" src=\"https://s3.envato.com/files/30191147/Preview%20(0.00.00.00).jpg\" alt=\"Card image cap\">"
-                    row += "<div>" + "Create Stage" + "<br><button class='btn btn-primary' onclick='GoToCreateStage()'>Create</button></div>"
-                    row += "<div>" + "ManageUsers" + "<br><button class='btn btn-primary' onclick='GoToManageUsers()'>Manage users</button></div>"
+                    row += "<div>" + "Create Stage" + "<br><button class='btn' onclick='GoToCreateStage()'>Create</button></div>"
+                    row += "<div>" + "ManageUsers" + "<br><button class='btn' onclick='GoToManageUsers()'>Manage users</button></div>"
                     row += "</div>"
                     row += "</div>"
                 }
